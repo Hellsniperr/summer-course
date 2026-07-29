d09275b23f8f7025e95fab39d94b8e566c6a3307
Hellsniperr <cbereza93@gmail.com>
2026-07-29 10:07:29 -0400

    adding stuff

--- Python/Lesson 07 - Data Structures/basic_algos.py
+++ Python/Lesson 07 - Data Structures/basic_algos.py
@@ -5,27 +5,30 @@
 # What is the output of this block of code?
 
 
-def mut_example(list1, list2, list3):
-    if len(list1) > 2:
-        list1 = list1[:2]
-    list2[0] = "hi"
-    list3 = "".join(list2)
+# def mut_example(list1, list2, list3):
+#     if len(list1) > 2:
+#         list1 = list1[:2]
+#     list2[0] = "hi"
+#     list3 = "".join(list2)
 
-a_list = [1, 2, 3]
-b_list = ["a", "b", "c"]
-a_str = "do-re-mi"
-mut_example(a_list, b_list, a_str)
-print(a_list)
-print(b_list)
-print(a_str)
+# a_list = [1, 2, 3]
+# b_list = ["a", "b", "c"]
+# a_str = "do-re-mi"
+# mut_example(a_list, b_list, a_str)
+# print(a_list)
+# print(b_list)
+# print(a_str)
 
-
+#1, 2
+# hi, b, c
+# hi, b, c, "doi-re-mi"
 
 
 # Exercise 2
 
 # What's the difference between sort and sorted?
-
+# .sort() is used for a lists only and changes the original list
+# sorted() changes the iterable and returns a new object without altering the original
 # Which one is a list method and which one is a function that works on lists?
 
 # Please explain
@@ -36,6 +39,18 @@ print(a_str)
 
 # Write a function that doubles the elements in a list.
 
+# def doubl_list(my_list):
+#     dbl_lst = []
+#     for x in my_list:
+#         dbl_lst.append(x * 2)
+        
+#     return dbl_lst
+
+
+# list_orig = [1, 2, 3, 4]
+# result = doubl_list(list_orig)
+
+# print(result)
 
 # Do you need to return anything here?
 
@@ -43,8 +58,14 @@ print(a_str)
 
 # Write a function that doubles the elements in a tuple.
 
-
-
+def dbl_tuple(my_tuple):
+    tuple1 = ()
+    for x in my_tuple:
+        tuple1 = tuple1 + (x * 2,)
+        
+    return tuple1
+my_tup = (1, 2, 3, 4)
+print(dbl_tuple(my_tup))
 # Do you need to return anything here?
 
 
@@ -53,7 +74,32 @@ print(a_str)
 
 # Rewrite the pop, count, extend, reverse, and sort functions
 
+def my_pop(in_list):
+    new_val = in_list[index]
+    del in_list[index]
+    return new_val
 
+
+def my_len(in_list):
+    len = 0
+    for elem in in_list:
+        len += 1
+    return len
+
+def my_count(in_list, obj):
+    count = 0
+    
+    
+    
+def my_extend(in_list, other_lst):
+    for elem in other_lst:
+        in_list.append(elem)
+        
+def my_reverse(in_list):
+    reversed = []
+    for elem in in_list[::-1]:
+        reversed.append(elem)
+    return reversed
 # Return the results in a new list and do not modify the original list
 
 # (do not use the function you are rewriting)
---
+++ Python/Lesson 07 - Data Structures/class_notes.py
@@ -0,0 +1,57 @@
+#preClass Problem Self
+
+# import random
+
+# sig = random.randint(1, 100)
+
+# its = 5
+
+# sig_list = []
+
+# def signal_decrypt(Value1: int, Value2: int, Value3:int, Value4:int, Value5: int):
+#     for i in sig:
+
+
+
+#     Value1 = int(input(""))
+#     Value2 = int(input(""))
+#     Value3 = int(input(""))
+#     Value4 = int(input(""))
+#     Value5 = int(input(""))
+
+#     return
+
+########## Pre-class problem Instructor 
+
+# signals = []
+
+# with open("preclass_prob1_data.txt", "r") as in_file:
+#     for line in in_file:
+#         signal = int(line)
+#         signals.append(signal)
+# signals_sorted = sorted(signals, reverse=True)
+# high_5 = signals_sorted[:5]
+# coordinate = sum(high_5) / 10.0
+# print(f"The coordinate is {coordinate}")
+
+
+# #Solution 2
+
+# with open("preclass_prob1_data.txt", "r") as in_file:
+#     print(f"The coordinate is {sum(sorted(int(x) for x in in_file)[-5]) / 10}")
+
+# my_list = list("hello")
+# print(my_list)
+## Hands-on 2
+# unit = {}
+
+# unit["john"] = {"rank": "pfc", "years_service": 1}
+# unit["mike"] = {"rank": "spc", "years_service": 2}
+# unit["luke"] = {"rank": "sft", "years_service": 3}
+# unit["matt"] = {"rank": "ssg", "years_service": 4}
+# unit["mark"] = {"rank": "sfc", "years_service": 5}
+
+
+
+# def SM_lookup(unit: str, sm_name: str):
+    
